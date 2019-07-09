78151ec55ffbef61d7ce549892dafb9ee8d63204
zhangjiahuashan <1191547491@qq.com>
2019-07-09 13:22:57 +0800

    分割线

--- module-health-manager/src/main/java/com/gcml/module_health_manager/MainHealthManagerFragment.java
+++ module-health-manager/src/main/java/com/gcml/module_health_manager/MainHealthManagerFragment.java
@@ -4,39 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.gcml.module_health_manager.activity.ContractManageActivity;
 import com.gcml.module_health_manager.api.HealthManagerRouterApi;
-import com.gcml.module_health_manager.bean.MenuBena;
 import com.gzq.lib_resource.mvp.StateBaseFragment;
 import com.gzq.lib_resource.mvp.base.IPresenter;
 import com.sjtu.yifei.annotation.Route;
 import com.sjtu.yifei.route.Routerfit;
-
-import java.util.ArrayList;
 
 /**
  * Created by gzq on 19-2-3.
  */
 @Route(path = "/healthmanager/main")
 public class MainHealthManagerFragment extends StateBaseFragment {
-    private ArrayList<MenuBena> menuTop = new ArrayList<MenuBena>() {{
-        add(new MenuBena(R.drawable.healthmanager_ic_health_task, "药物化合物查询"));
-        add(new MenuBena(R.drawable.healthmanager_ic_health_report, "疾病症状查询"));
-        add(new MenuBena(R.drawable.healthmanager_ic_risk_assessment, "基因查询"));
-        add(new MenuBena(R.drawable.healthmanager_ic_health_measure, "通用关系查询"));
-    }};
-    private ArrayList<MenuBena> menuBottom = new ArrayList<MenuBena>() {
-        {
-            add(new MenuBena(R.drawable.healthmanager_ic_health_measure, "健康管理审批"));
-            add(new MenuBena(R.drawable.healthmanager_ic_family_doctor, "家庭医生服务"));
-            add(new MenuBena(R.drawable.healthmanager_ic_family_doctor, "医学文献"));
-        }
-    };
     private View view;
 
     @Nullable
